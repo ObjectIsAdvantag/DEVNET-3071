532b45ac83d3712617947ce9203547fbbefda238
ObjectIsAdvantag <steve.sfartz@gmail.com>
2017-06-26 11:33:35 -0700

    added step by step tuto for DEVNET-3071

---
+++ template/steps/step1.js
@@ -0,0 +1,47 @@
+//
+// coffee: example of a conversation with yes/no options
+//
+// "What about coffee (yes / no / cancel)"
+//
+module.exports = function (controller) {
+
+    controller.hears(['start'], 'direct_message,direct_mention', function (bot, message) {
+
+        bot.createConversation(message, function (err, convo) {
+
+            convo.ask("Ready for a challenge (yes/no/cancel)", [
+                {
+                    pattern: "yes|yeh|sure|oui|si",
+                    callback: function (response, convo) {
+                        convo.say("Let's start");
+                        convo.next();
+                    },
+                }
+                , {
+                    pattern: "no|neh|non|na|birk",
+                    callback: function (response, convo) {
+                        convo.say("Too bad, looking forward to play with you");
+                        convo.next();
+                    },
+                }
+                , {
+                    pattern: "cancel|stop|exit",
+                    callback: function (response, convo) {
+                        convo.say("Got it, cancelling...");
+                        convo.next();
+                    },
+                }
+                , {
+                    default: true,
+                    callback: function (response, convo) {
+                        convo.say("Sorry, I did not understand.");
+                        convo.repeat();
+                        convo.next();
+                    }
+                }
+            ]);
+
+            convo.activate();
+        });
+    });
+};
---
+++ template/steps/step6.js
@@ -0,0 +1,102 @@
+//
+// coffee: example of a conversation with yes/no options
+//
+// "What about coffee (yes / no / cancel)"
+//
+module.exports = function (controller) {
+
+    controller.hears(['start'], 'direct_message,direct_mention', function (bot, message) {
+
+        bot.createConversation(message, function (err, convo) {
+
+            // Default thread
+            convo.ask("Ready for a challenge (yes/no/cancel)", [
+                {
+                    pattern: "yes|yeh|sure|oui|si",
+                    callback: function (response, convo) {
+
+                        // Apply elaps time (in milliseconds) to next askQuestion
+                        convo.setTimeout(5000);
+                        convo.onTimeout(function (convo) {
+                            convo.gotoThread("missed");
+                        });
+
+                        convo.gotoThread('quiz');
+                    },
+                }
+                , {
+                    pattern: "no|neh|non|na|birk",
+                    callback: function (response, convo) {
+                        convo.say("Too bad, looking forward to play with you later...");
+                        convo.next();
+                    },
+                }
+                , {
+                    pattern: "cancel|stop|exit",
+                    callback: function (response, convo) {
+                        convo.gotoThread('cancel');
+                    },
+                }
+                , {
+                    default: true,
+                    callback: function (response, convo) {
+                        convo.say("Sorry, I did not understand.");
+                        convo.repeat();
+                        convo.next();
+                    }
+                }
+            ]);
+
+            // Cancel thread
+            convo.addMessage({
+                text: "Got it, cancelling...",
+                action: 'stop', // this marks the converation as unsuccessful
+            }, 'cancel');
+
+            // Quiz thread
+            convo.addMessage("Let's start", "quiz");
+
+            var challenge = pickChallenge();
+            convo.addQuestion("Question: " + challenge.question, [
+                {
+                    pattern: "^"+ challenge.answer + "$",
+                    callback: function (response, convo) {
+                        convo.gotoThread('success');
+                    },
+                }
+                , {
+                    pattern: "cancel|stop|exit",
+                    callback: function (response, convo) {
+                        convo.gotoThread('cancel');
+                    },
+                }
+                , {
+                    default: true,
+                    callback: function (response, convo) {
+                        convo.say("Sorry, wrong answer. Try again!");
+                        convo.repeat();
+                        convo.next();
+                    }
+                }
+            ], {}, 'quiz');
+
+            // Succes thread
+            convo.addMessage("Congrats, you did it!", "success");
+
+            // Succes thread
+            convo.addMessage("Too bad, you missed this one", "missed");
+
+            convo.activate();
+        });
+    });
+};
+
+
+function pickChallenge() {
+    var a = Math.round(Math.random()*5) + 4;
+    var b = Math.round(Math.random()*5) + 4;
+    return {
+        question : "" + a + " x " + b + " =",
+        answer : "" + (a * b)
+    }
+}
